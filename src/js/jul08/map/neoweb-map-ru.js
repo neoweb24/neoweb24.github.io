81929df387950698bd342fa0c977808781bd5a83
neowebkyiv <web-design@ukr.net>
2025-08-08 22:32:54 +0300

    7.27. Design 4 - Update [neoweb.kyiv.ua]

--- src/js/jul08/map/neoweb-map-ru.js
+++ src/js/jul08/map/neoweb-map-ru.js
@@ -1,8 +1,10 @@
-const mapViewN = 50.452761
-const mapViewE = 30.470066
+const markerN = 50.451373695537086
+const markerE = 30.468277280288355
+const mapOffset = 0.003301 // смещение для ru версии
+const mapViewN = markerN + mapOffset
+const mapViewE = markerE
 const mapViewZoom = 14
-const markerN = 50.44946
-const markerE = mapViewE
+const locationDescription = '<b>Neoweb</b><br>Украина, Киев<br>просп. Берестейский 26<br>(район КПИ, ст. метро Политехнический институт)<br><a href="https://neoweb.kyiv.ua" title="Создание сайтов, создание Интернет магазинов">neoweb.kyiv.ua</a>'
 
 const map = L.map('map').setView([mapViewN, mapViewE], mapViewZoom);
 
@@ -12,7 +14,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const marker = L.marker([markerN, markerE]).addTo(map)
-    .bindPopup('<b>Neoweb</b><br>Украина, Киев<br>просп. Берестейский 25<br>(район КПИ, ст. метро Политехнический институт)<br><a href="https://neoweb.kyiv.ua" title="Создание сайтов, создание Интернет магазинов">neoweb.kyiv.ua</a>').openPopup();
+    .bindPopup(locationDescription).openPopup();
 
 // function onMapClick(e) {
 // 	popup
@@ -20,11 +22,11 @@ const marker = L.marker([markerN, markerE]).addTo(map)
 // 		.setContent(`Вы нажали на карту в таких координатах: ${e.latlng.toString()}`)
 // 		.openOn(map);
 // }
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
 
-map.on('click', onMapClick);
+// map.on('click', onMapClick);
--- src/js/jul08/map/neoweb-map-ua.js
+++ src/js/jul08/map/neoweb-map-ua.js
@@ -1,8 +1,10 @@
-const mapViewN = 50.451858
-const mapViewE = 30.470066
+const markerN = 50.451373695537086
+const markerE = 30.468277280288355
+const mapOffset = 0.002398 // смещение для UA версии
+const mapViewN = markerN + mapOffset
+const mapViewE = markerE
 const mapViewZoom = 14
-const markerN = 50.44946
-const markerE = mapViewE
+const locationDescription = '<b>Neoweb</b><br>Україна, Київ<br>просп. Берестейський 26<br>(район КПІ, ст. метро Політехнічний інститут)<br><a href="https://neoweb.kyiv.ua" title="Створення сайтів, створення Інтернет магазинів">neoweb.kyiv.ua</a>'
 
 const map = L.map('map').setView([mapViewN, mapViewE], mapViewZoom);
 
@@ -12,7 +14,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const marker = L.marker([markerN, markerE]).addTo(map)
-    .bindPopup('<b>Neoweb</b><br>Україна, Київ<br>просп. Берестейський 25<br>(район КПІ, ст. метро Політехнічний інститут)<br><a href="https://neoweb.kyiv.ua" title="Створення сайтів, створення Інтернет магазинів">neoweb.kyiv.ua</a>').openPopup();
+    .bindPopup(locationDescription).openPopup();
 
 // function onMapClick(e) {
 // 	popup
@@ -20,11 +22,11 @@ const marker = L.marker([markerN, markerE]).addTo(map)
 // 		.setContent(`Вы нажали на карту в таких координатах: ${e.latlng.toString()}`)
 // 		.openOn(map);
 // }
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
 
-map.on('click', onMapClick);
+// map.on('click', onMapClick);
